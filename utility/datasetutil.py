bca16918b5bbf523161923bcc4cfc41389234f21
junseo0421 <jmdmk9st@naver.com>
2024-11-11 14:56:57 +0900

    register 수정

--- utility/datasetutil.py
+++ utility/datasetutil.py
@@ -70,6 +70,7 @@ def authentic_ds(csvname):
     register_ds=csv2list(csvname)
     for i,x in enumerate(register_ds):
         register_ds[i][0]=0
+        register_ds[i][1]=join("/content/", register_ds[i][1])
     return register_ds
 
 
